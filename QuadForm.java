d0813f8676788cdd9df6bcde9554978568676541
Nick Keirstead <nicholas_keirstead@horacemann.org>
2017-04-16 23:57:34 -0400

    Quadratic Formula

--- QuadForm.java
+++ QuadForm.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class QuadForm {
     public static String findRoots(double a, double b, double c) {
         double discrim = b*b - 4*a*c;
@@ -9,13 +11,20 @@ public class QuadForm {
             double x1 = (-b + Math.sqrt(discrim)) / (2*a);
             double x2 = (-b - Math.sqrt(discrim)) / (2*a);
             if (discrim == 0) {
-                return "There is a double root at " + x1;
+                return "There is a double root at x = " + x1;
             } else {
                 return "The roots are x = " + x1 + " and x = " + x2;
             }
         }
     }
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a, b, c: ");
+        String[] abcStrs = s.nextLine().split(",");
+        double[] abc = new double[3];
+        for (int i = 0; i < 3; i++) {
+            abc[i] = Double.valueOf(abcStrs[i].trim());
+        }
+        System.out.println(findRoots(abc[0], abc[1], abc[2]));
     }
 }
